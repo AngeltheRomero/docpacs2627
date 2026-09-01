c69efac362ca0b46ab3ba0e8ec3630c42e58ed03
AngeltheRomero <28ARomero@live.ytech.edu>
2026-09-01 14:56:10 -0400

    more work in progress

--- docpac_030309/Button Masher Game/RomeroAngel/game.js
+++
@@ -1,8 +0,0 @@
-let timer = document.getElementById("timeBox")
-
-function timer() {
-    timeLeft--;
-    timer.innerText = timeLeft;
-    document.getElementById("timeBox")
-    if (timeLeft === 0) { }
-}
