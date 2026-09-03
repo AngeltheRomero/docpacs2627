34b0556c820d67055d5eee827b667f6e893744e2
AngeltheRomero <28ARomero@live.ytech.edu>
2026-09-02 21:06:39 -0400

    Huge Changes (A Button works!!!)

--- docpac_030309/Button Masher Game/RomeroAngel/game.js
+++ docpac_030309/Button Masher Game/RomeroAngel/game.js
@@ -1,3 +1,52 @@
+let score = 0;
+let aWasPressedBefore = false;
+let activeGamepadIndex = null;
+
+window.addEventListener('gamepadconnected', function (e) {
+    console.log('Controller connected!', e.gamepad);
+    activeGamepadIndex = e.gamepad.index;
+    loop();
+});
+
+function loop() {
+    let gamepad = navigator.getGamepads()[activeGamepadIndex];
+
+    if (!gamepad) {
+        return;
+    }
+
+    let aIsPressedNow = gamepad.buttons[0].pressed;
+
+
+    console.log('Joystick X:', gamepad.axes[0], 'Joystick Y:', gamepad.axes[1]);
+
+    if (aIsPressedNow && !aWasPressedBefore) {
+        score = score + 1;
+        let scoreBox = document.getElementById('scoreBox')
+        scoreBox.textContent = "Score: " + score;
+    }
+    if (gamepad.axes[0] < -0.5) {
+        console.log('Holding LEFT!');
+    }
+
+    if (gamepad.axes[0] > 0.5) {
+        console.log('Holding RIGHT!');
+    }
+
+    if (gamepad.axes[1] < -0.5) {
+        console.log('Holding UP!');
+    }
+
+
+    if (gamepad.axes[1] > 0.5) {
+        console.log('Holding DOWN!');
+    }
+
+    aWasPressedBefore = aIsPressedNow;
+
+    requestAnimationFrame(loop);
+}
+
 let timeLeft = 20;
 
 const timerElement = document.getElementById('timeBox');
@@ -10,19 +59,3 @@ const countdownInterval = setInterval(() => {
         timerElement.textContent = "YOUR TIME IS UP!!!!!";
     }
 }, 1000);
-
-let currentScore = 0;
-
-const scoreBox = document.getElementById("scoreBox")
-function changeScore(amount) {
-    currentScore += amount;
-}
-
-window.addEventListener('gamepadconnected', function (e) {
-    console.log('Controller connected!', e.gamepad);
-});
-
-function loop() {
-    let gamepad = navigator.getGamepads()[activeGamepadIndex]
-}
-
